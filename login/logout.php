bb9013fb8b14a7464fa1d1f52e4b955bc35fbd58
mrrboto <mrrboto@gmail.com>
2017-04-08 16:41:23 -0700

    redirect change > 4/8/17@1640

--- login/logout.php
+++ login/logout.php
@@ -3,6 +3,6 @@ session_start();
 if(session_destroy()) // Destroying All Sessions
 {
     mysqli_close($connection);
-    header("Location: /Riskories/login/login.php"); // Redirecting To Home Page
+    header("Location: ../login/login.php"); // Redirecting To Home Page
 }
 ?>
