e2d64bf6dd436ad7dabaea3761483ef3df6b2b3b
srodewal <srodewal@asu.edu>
2017-05-03 16:11:33 -0700

    Registration now has error alerts. Added editConsentForm.php file

---
+++ consent/editConsentForm.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+<body>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <div class="container">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4>Edit Consent Form</h4></div>
+                  <div class="panel-body">
+                      <form>
+
+                      </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
