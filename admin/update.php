2170d417275556becea9b36a12d452dfd8e1b112
srodewal <srodewal@asu.edu>
2017-05-03 11:51:29 -0700

    Updates to admin->update.php. Still working on alerts for this page

--- admin/update.php
+++ admin/update.php
@@ -12,63 +12,73 @@
     <title>PHP</title>
 </head>
 <body style="background: #efefef;">
-<?php
+    <div class="container">
+        <?php
 
-  $name = '';
-  $gender = '';
+          $name = '';
+          $gender = '';
 
-  if (isset($_POST['submit'])) {
-    $ok = true;
-    if (!isset($_POST['name']) || $_POST['name'] === '') {
-        $ok = false;
-    } else {
-        $name = $_POST['name'];
-    }
-    if (!isset($_POST['gender']) || $_POST['gender'] === '') {
-        $ok = false;
-    } else {
-        $gender = $_POST['gender'];
-    }
-
-
-    if ($ok) {
-
-        $sql = sprintf("UPDATE users SET name='%s', gender='%s'
-          WHERE id=%s",
-          mysqli_real_escape_string($db, $name),
-          mysqli_real_escape_string($db, $gender),
-          $id);
-        mysqli_query($db, $sql);
-        echo '<p>User updated.</p>';
-        mysqli_close($db);
-    }
-  } else {
-
-      $sql = sprintf('SELECT * FROM users WHERE id=%s', $id);
-      $result = mysqli_query($db, $sql);
-      foreach ($result as $row) {
-          $name = $row['name'];
-          $gender = $row['gender'];
-      }
-      mysqli_close($db);
-  }
-?>
-<form method="post" action="">
-    User name: <input type="text" name="name" value="<?php
-        echo htmlspecialchars($name);
-    ?>"><br>
-    Gender:
-        <input type="radio" name="gender" value="f"<?php
-            if ($gender === 'f') {
-                echo ' checked';
+          if (isset($_POST['submit'])) {
+            $ok = true;
+            if (!isset($_POST['name']) || $_POST['name'] === '') {
+                $ok = false;
+            } else {
+                $name = $_POST['name'];
             }
-        ?>>female
-        <input type="radio" name="gender" value="m"<?php
-            if ($gender === 'm') {
-                echo ' checked';
+            if (!isset($_POST['gender']) || $_POST['gender'] === '') {
+                $ok = false;
+            } else {
+                $gender = $_POST['gender'];
             }
-        ?>>male<br>
-    <input type="submit" name="submit" value="Submit">
-</form>
+
+
+            if ($ok) {
+
+                $sql = sprintf("UPDATE users SET name='%s', gender='%s'
+                  WHERE id=%s",
+                  mysqli_real_escape_string($db, $name),
+                  mysqli_real_escape_string($db, $gender),
+                  $id);
+                mysqli_query($db, $sql);
+                echo '<p>User updated.</p>'; // <--
+                mysqli_close($db);
+            }
+          } else {
+
+              $sql = sprintf('SELECT * FROM users WHERE id=%s', $id);
+              $result = mysqli_query($db, $sql);
+              foreach ($result as $row) {
+                  $name = $row['name'];
+                  $gender = $row['gender'];
+              }
+              mysqli_close($db);
+          }
+        ?>
+        <div class="well">
+            <form method="post" action="">
+                <div class="form-group">
+                    <label for="name">Username: </label>
+                    <input class="form-control" type="text" name="name" value="<?php
+                        echo htmlspecialchars($name);
+                    ?>">
+                </div>
+                <div class="form-group">
+                    <label for="gender">Gender: </label>
+                    <input type="radio" name="gender" value="f"<?php
+                        if ($gender === 'f') {
+                            echo ' checked'; // <--
+                        }
+                    ?>>Female
+                    <input type="radio" name="gender" value="m"<?php
+                        if ($gender === 'm') {
+                            echo ' checked'; // <--
+                        }
+                    ?>>Male<br>
+                </div>
+
+                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
