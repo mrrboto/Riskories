43fac90c33bfc796d4b29828958be7c083429dcc
krast214 <krast214@gmail.com>
2017-05-03 22:20:48 -0700

    Changes to admin.php, excel.php, guests_excel.php
    Added Export Guest CSV button to admin.php.
    Added guests_excel.php similar to excel.php for guests table
    Changed excel.php to rename Data.csv to UserData.csv

---
+++ guests_excel.php
@@ -0,0 +1,27 @@
+<?php
+//KR Excel Export
+//Include config and db files for easy sql connection
+include('../db/config.php');
+include('../db/db.php');
+
+ session_start();
+
+ $sqli = 'SELECT * FROM guests';
+ $result = mysqli_query($db, $sqli);
+
+$filename = 'GuestData.csv';
+
+$fp = fopen("php://output", 'w');
+$header_flag = false;
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {  //MYSQLI_ASSOC is same as mysqli_fetch_assoc()
+    //add headers
+    if(!$header_flag) {
+        fputcsv($fp, array_keys($row));
+        $header_flag = true;
+    }
+    fputcsv($fp, array_values($row));
+}
+fclose($fp);
+//header("Location: ../admin/admin.php");
+header('Content-Type: application/csv');
+header('Content-Disposition: attachment; filename="'.$filename.'";');
