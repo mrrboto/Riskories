cb4cf5dab7af0aab34d28b36d88ce0aafe508af8
srodewal <srodewal@asu.edu>
2017-05-03 22:54:04 -0700

    Added bootstrap links and made small style changes.

--- questions/postQ.php
+++ questions/postQ.php
@@ -105,6 +105,15 @@
     </head>
     <body>
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
         <div class="container">
             <div class="row">
@@ -163,7 +172,7 @@
                     <div class="form-group">
                       <label for="comments">Comments:</label><br>
                      <ul>
-                        <textarea name="comments" id="comments" cols="55" rows="5"></textarea><br>
+                        <textarea class="form-control" name="comments" id="comments" cols="55" rows="5"></textarea><br>
                     </ul>
                     </div>                       
 
--- questions/preQ.php
+++ questions/preQ.php
@@ -102,6 +102,15 @@ $preQ1Err = $preQ2Err = $preQ3Err = "";
     </head>
     <body>
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
         <div class="container">
             <div class="row">
